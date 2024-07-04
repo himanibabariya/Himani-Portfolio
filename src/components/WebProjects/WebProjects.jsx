c996ce100f5ce3901fc491bae26b560986ee07cb
Himani Babariya <himanibabariya@gmail.com>
2024-07-03 22:25:27 -0400

    More Projects Added

--- src/components/WebProjects/WebProjects.jsx
+++ src/components/WebProjects/WebProjects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Laptop from "../../assets/images/laptop.png";
 import Project3 from "../../assets/videos/project3.png";
+import VehicleLoanCalculator from "../../assets/videos/VehicleLoanCalculator.png"
 import star from "../../assets/other/star.svg"
 import react from "../../assets/technologies/react.svg"
 import tailwind from "../../assets/technologies/tailwind.svg"
@@ -10,6 +11,8 @@ import firebase from "../../assets/technologies/firebase.svg"
 const WebProjects = () => {
   return (
     <div>
+
+      {/* Parcel Tracker Project  */}
       <div className="project-container">
         <div className="left">
           <p className='title'>Project Name:</p>
@@ -54,11 +57,11 @@ const WebProjects = () => {
             </div>
             <div className="item">
               <img src={star} alt="" />
-              <p>Prototype</p>
+              <p>Reliable</p>
             </div>
             <div className="item">
               <img src={star} alt="" />
-              <p>Prototype</p>
+              <p>Dynamic</p>
             </div>
           </div>
 
@@ -67,6 +70,58 @@ const WebProjects = () => {
       </div>
 
       <hr />
+
+      {/* Vehicle Loan Calculator  */}
+      <div className="project-container">
+        <div className="left">
+          <p className='title'>Project Name:</p>
+          <p className='project-name'>VehicleLoanCalculator</p>
+
+          <div className="laptop">
+            <img src={Laptop} alt="" className='img1' />
+            <img src={VehicleLoanCalculator} alt="" className='projectImg' />
+          </div>
+
+          <div className="tech-section">
+              <p>Technologies used :</p>
+            <div className="technologies">
+              <img src={react} alt="" />
+              <p>React Js</p>
+              <img src={tailwind} alt="" />
+              <p>Tailwind</p>
+
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <p className='des'>Description:</p>
+          <p className='description'>The "Vehicle Loan Calculator" is a web application built using React and Tailwind, designed to simplify the process of calculating loan payments for vehicles. It provides users with an intuitive interface to input loan details such as the Vehicle amount,Down Payment, interest rate, and loan term. The calculator then instantly generates the monthly payment amount, helping users make informed financial decisions and plan their budgets effectively.</p>
+
+          <div className="liveProject">
+            <a href="https://github.com/himanibabariya/Vehicle-Loan-Calculator" target='_blank'><button className="btn-orange">Github</button></a>
+            <a href="https://vehicleloancalculator.netlify.app/" target='_blank'><button className="btn-blue">Live Demo</button></a>
+          </div>
+
+
+          <div className="concepts">
+            <div className="item">
+              <img src={star} alt="" />
+              <p>User-friendly interface</p>
+            </div>
+            <div className="item">
+              <img src={star} alt="" />
+              <p>Real-time calculations</p>
+            </div>
+            
+          </div>
+
+
+        </div>
+      </div>
+
+      <hr />
+
+
 
     </div>
   )
